9976b350307d3ad23823b101bf653e509c056971
gitsuguru <sugusugu.ryouri@gmail.com>
2021-08-08 22:17:53 +0900

    pwa設定

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", function (event) {
+    console.log("[ServiceWorker] Install");
+});
+
+self.addEventListener("activate", function (event) {
+    console.log("[ServiceWorker] Activate");
+});
+
+self.addEventListener("fetch", function (event) {});
