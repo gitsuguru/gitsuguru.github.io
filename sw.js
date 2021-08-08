b7f8ae6d71daa251337b11472dcb9321034679a2
gitsuguru <sugusugu.ryouri@gmail.com>
2021-08-09 00:20:07 +0900

    pwaのcache3

--- sw.js
+++ sw.js
@@ -1,26 +1,31 @@
-const CACHE_NAME = 'sampleCache';
-const urlsToCache = [
+// cacheしたいファイルを定義
+const cache = [
     "index.html",
     "./main.css",
     "./p.html"
 ];
 
 self.addEventListener("install", function (event) {
+    // 引数の処理が成功したかどうかを判定
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log("Opened cache");
+        // 任意のcacheを定義
+        caches.open("sampleCache").then(function (cache) {
+            // cacheにファイルを定義
             return cache.addAll(urlsToCache);
         })
     );
 });
 
 self.addEventListener("fetch", function (event) {
+    // fetchハンドリングを抑止(Response用のPromiseを定義)
     event.respondWith(
+        // レスポンスの中にcacheと一致するものがあるか判定
         caches.match(event.request).then(function (response) {
-            // Cache hit - return response
             if (response) {
+                // cacheを返却
                 return response;
             }
+            // レスポンスを返却
             return fetch(event.request);
         })
     );
