a512bd74797508e0519473c15f34da155dd699f4
gitsuguru <sugusugu.ryouri@gmail.com>
2021-08-08 23:11:44 +0900

    pwaのcache

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
+const CACHE_NAME = 'sampleCache';
+const urlsToCache = [
+    "/",
+    "./main.css",
+    "./p.html"
+];
+
 self.addEventListener("install", function (event) {
-    console.log("[ServiceWorker] Install");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener("activate", function (event) {
-    console.log("[ServiceWorker] Activate");
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
 });
-
-self.addEventListener("fetch", function (event) {});
