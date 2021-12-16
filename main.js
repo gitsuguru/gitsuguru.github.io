86e0bf67316afc9c630fdec00fdf601158d57ff2
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-16 21:36:27 +0900

    push通知2

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 
+let result = '';
+if (Notification.permission == "default") {
+    Notification.requestPermission();
+} else if (Notification.permission == "granted") {
+    result = true;
+} else {
+    result = false;
+}
 window.addEventListener("blur", () => {
-    push();
-});
-
-const push = () => {
-    if (Notification.permission == "default") {
-        Notification.requestPermission();
-    } else if (Notification.permission == "granted") {
-        new Notification('通知成功');
+    if (result) {
+        push();
     } else {
         alert('通知が許可されていません');
     }
+});
+
+const push = () => {
+    new Notification("通知成功");
 };
