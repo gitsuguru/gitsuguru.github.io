0650da937bcee29c227dd8828af74c9f63c9167b
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-16 21:30:19 +0900

    push通知

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("worker.js")
-        .then((register) => {
-            console.log("登録に成功しました。：" + register);
-        })
-        .catch((error) => {
-            console.log("登録に失敗しました。：" + error);
-        });
-}
+
+window.addEventListener("blur", () => {
+    push();
+});
+
+const push = () => {
+    if (Notification.permission == "default") {
+        Notification.requestPermission();
+    } else if (Notification.permission == "granted") {
+        new Notification('通知成功');
+    } else {
+        alert('通知が許可されていません');
+    }
+};
