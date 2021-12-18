14c4acc31171f9c638d0cd0226da391f884230fd
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-18 18:47:04 +0900

    push通知修正1

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const btn = document.querySelector('button');
 btn.addEventListener("click", () => {
-    console.log(1);
     push();
 });
 
-function push () {
-    console.log(Notification.permission);
-    if (Notification.permission == "default") {
-        Notification.requestPermission();
-        console.log(3);
-    } else if (Notification.permission == "granted") {
-        new Notification("通知成功");
+function push() {
+    // ブラウザが通知をサポートしているか確認
+    if (!("Notification" in window)) {
+        alert("通知に対応していないブラウザです。");
     } else {
-        console.log(4);
+        if (Notification.permission === "default") {
+            Notification.requestPermission();
+        } else if (Notification.permission === "granted") {
+            new Notification("通知成功");
+        } else {
+            alert("通知が許可されていません。");
+        }
+        alert("1");
     }
 };
 
