01ef64051e26fc467a2f6cdcdfc85ef047c263fc
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-19 17:40:43 +0900

    push通知修正9

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ btn.addEventListener("click", () => {
 });
 function push() {
     if (("Notification" in window) && Notification.permission === "granted") {
-        new Notification('こんにちは');
+        new Notification("Push通知", {
+            body: "通知が送信されました。",
+            image: './img/soccer.png'
+        });
     } else {
         alert("通知に対応していないブラウザか、通知が許可されていません。");
     }
