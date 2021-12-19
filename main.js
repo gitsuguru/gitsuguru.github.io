ff96e5187dcaeea19f68dbc02947bb036fd11d0c
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-19 17:45:14 +0900

    push通知修正10

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+// ボタンを取得
 const btn = document.querySelector("button");
+// ページ表示時にプッシュ通知許可を得る
 window.addEventListener("load", () => {
     if (("Notification" in window) && Notification.permission === "default") {
         Notification.requestPermission();
     }
 });
+// ボタンがクリックされた際にpush関数を実行
 btn.addEventListener("click", () => {
     push();
 });
+// プッシュ通知が許可されていれば送信する
 function push() {
     if (("Notification" in window) && Notification.permission === "granted") {
-        new Notification("Push通知", {
-            body: "通知が送信されました。",
-            image: './img/soccer.png'
+        new Notification("送信されました", {
+            body: "プッシュ通知が送信されました。",
         });
     } else {
         alert("通知に対応していないブラウザか、通知が許可されていません。");
