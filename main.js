c8c47a558ad6866f9fd433cba2e8a6290b195b09
gitsuguru <sugusugu.ryouri@gmail.com>
2021-12-27 16:56:26 +0900

    cookie

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-// ボタンを取得
-const btn = document.querySelector("button");
-// ページ表示時にプッシュ通知許可を得る
-window.addEventListener("load", () => {
-    if ("Notification" in window && Notification.permission === "default") {
-        Notification.requestPermission();
-    }
-});
-// ボタンがクリックされた際にpush関数を実行
-btn.addEventListener("click", () => {
-    push();
-});
-// プッシュ通知が許可されていれば送信する
-function push() {
-    if ("Notification" in window && Notification.permission === "granted") {
-        new Notification("送信されました", {
-            body: "プッシュ通知が送信されました。",
-        });
-    } else {
-        alert("通知に対応していないブラウザか、通知が許可されていません。");
-    }
-}
+// // ボタンを取得
+// const btn = document.querySelector("button");
+// // ページ表示時にプッシュ通知許可を得る
+// window.addEventListener("load", () => {
+//     if ("Notification" in window && Notification.permission === "default") {
+//         Notification.requestPermission();
+//     }
+// });
+// // ボタンがクリックされた際にpush関数を実行
+// btn.addEventListener("click", () => {
+//     push();
+// });
+// // プッシュ通知が許可されていれば送信する
+// function push() {
+//     if ("Notification" in window && Notification.permission === "granted") {
+//         new Notification("送信されました", {
+//             body: "プッシュ通知が送信されました。",
+//         });
+//     } else {
+//         alert("通知に対応していないブラウザか、通知が許可されていません。");
+//     }
+// }
+
+const cookie = document.querySelector('h1');
+
+// cookieに保存
+document.cookie = 'age = 27'; // ①
+document.cookie = `name = ${encodeURIComponent('すぐる')}`; // ②
