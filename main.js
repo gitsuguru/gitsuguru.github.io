87659c5c807865cc4c33bf0f4f5ccb9fe9d6670a
gitsuguru <sugusugu.ryouri@gmail.com>
2022-01-25 22:16:38 +0900

    ESモジュール

--- main.js
+++ main.js
@@ -1,47 +1,16 @@
-// // ボタンを取得
-// const btn = document.querySelector("button");
-// // ページ表示時にプッシュ通知許可を得る
-// window.addEventListener("load", () => {
-//     if ("Notification" in window && Notification.permission === "default") {
-//         Notification.requestPermission();
-//     }
-// });
-// // ボタンがクリックされた際にpush関数を実行
-// btn.addEventListener("click", () => {
-//     push();
-// });
-// // プッシュ通知が許可されていれば送信する
-// function push() {
-//     if ("Notification" in window && Notification.permission === "granted") {
-//         new Notification("送信されました", {
-//             body: "プッシュ通知が送信されました。",
-//         });
-//     } else {
-//         alert("通知に対応していないブラウザか、通知が許可されていません。");
-//     }
-// }
+import { variable } from "./sub.js";
+import { object } from "./sub.js";
+import { testClass } from "./sub.js";
+import func from "./sub.js";
 
-const cookie = document.querySelector('h1');
+console.log(variable);
+console.log(object);
+new testClass().testMethod();
 
-// cookieに保存
-document.cookie = 'age = 27'; // ①
-document.cookie = `name = ${encodeURIComponent('すぐる')}`; // ②
+func();
 
-// cookieの取得
-const cookies = document.cookie;
-// ;毎に文字列を分割
-const cookieArray = cookies.split(";");
-// オブジェクトに変型
-let obj = {};
-cookieArray.forEach((element) => {
-    // =毎に文字列を分割
-    const itemArray = element.split("=");
-    // 文字列間の空白を削除
-    const key = itemArray[0].trim();
-    // デコード
-    const value = decodeURIComponent(itemArray[1]);
-    // オブジェクトに追加
-    obj[key] = value;
-});
 
-console.log(obj);
+
+
+// HTML type指定
+// ローカル環境×
---
+++ sub.js
@@ -0,0 +1,12 @@
+export let variable = "変数";
+export const object = { name: "オブジェクト" };
+export class testClass {
+    testMethod() {
+        console.log("クラス");
+    }
+}
+
+function testFunction() {
+    console.log("関数");
+}
+export default testFunction;
